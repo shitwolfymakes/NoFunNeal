7990d9451c24615d497fc000da9178634181c368
wolfy <wolfy@shitwolfymakes.com>
2024-02-26 04:17:14 -0600

    added a loop to restart it after "connection reset by peer"

--- run_agent.sh
+++ run_agent.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-go run main.go ./secrets/ > /dev/null 2>&1 &
+
+# Set a flag to indicate whether the loop should continue
+RUNNING=true
+
+# Function to handle Ctrl+C
+function ctrl_c() {
+    echo "Exiting..."
+    RUNNING=false
+}
+
+# Trap Ctrl+C and call the function to handle it
+trap ctrl_c SIGINT
+
+# Infinite loop
+while $RUNNING; do
+    # Your code here
+    go run main.go ./secrets/
+    sleep 120
+done
